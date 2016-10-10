af06fbb7f64bd170a78d92d72d621dbc9811bf1b
Brian Altman <balt490@gmail.com>
2016-10-10 13:35:33 -0400

    Homepage, social bar updated

--- main.rb
+++ main.rb
@@ -1,1 +1,17 @@
 require 'sinatra'
+
+post '/' do
+@title = "Home"
+end
+
+post '/checkout' do
+@title = "Checkout"	
+end
+
+post '/shop' do
+@title = "Shop"
+end
+
+post '/contact' do
+@title = "Contact"
+end
---
+++ public/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+var imageRotator = {
+	
+	initialze: function() {
+
+		var itemCount = $(".rotatingimage").length;
+
+		var currentItem = 0;
+
+		$(".rotatingimage").eq(currentItem).fadeIn(1000);
+
+		var loop = setInterval(function(){
+			$(".rotatingimage").eq(currentItem).fadeOut(2500);
+			if(currentItem == itemCount -1){
+				currentItem = 0;
+			} else {
+				currentItem++;
+			}
+			$(".rotatingimage").eq(currentItem).fadeIn(2500);
+			}, 5000);
+		}
+	};
+
+imageRotator.initialze();
+
+});
